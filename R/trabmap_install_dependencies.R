d6e20172c37ffe00980baafd50889101f0ade755
Jaap Saers <jaap.saers@naturalis.nl>
2024-09-30 13:46:06 +0200

    added install dependencies function

--- R/trabmap_install_dependencies.R
+++ R/trabmap_install_dependencies.R
@@ -1,4 +1,4 @@
-function(){
+trabmap_install_dependencies <- function(){
 
   if(length(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse") %in% installed.packages()[,"Package"]]) > 0){
     install.packages(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse") %in% installed.packages()[,"Package"]])
