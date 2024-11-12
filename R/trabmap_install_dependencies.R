e93fe4ee543bf198cda2217ceaee674a52c8ccce
Jaap Saers <jaap.saers@naturalis.nl>
2024-11-12 17:16:27 +0100

    removed functions ralting to ply meshes

--- R/trabmap_install_dependencies.R
+++ R/trabmap_install_dependencies.R
@@ -1,7 +1,7 @@
 trabmap_install_dependencies <- function(){
 
-  if(length(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse", "Rvcg")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse", "Rvcg") %in% installed.packages()[,"Package"]]) > 0){
-    install.packages(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse", "Rvcg")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse", "Rvcg") %in% installed.packages()[,"Package"]])
+  if(length(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse") %in% installed.packages()[,"Package"]]) > 0){
+    install.packages(c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse")[!c("EBImage", "tiff", "oro.nifti", "foreach", "mmand", "doParallel", "BiocManager", "iterators", "parallel", "doSNOW", "progress", "plyr", "tidyverse") %in% installed.packages()[,"Package"]])
   }
 
   remotes::install_github("AlessioVeneziano/IndianaBones")
